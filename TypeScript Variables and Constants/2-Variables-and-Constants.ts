6b80198a7abcdecbbb5581ed622787f28dd31c13
Arafat Shalehin <md.arafatshalehin@gmail.com>
2026-04-27 20:29:13 +0600

    variables & constants

---
+++ TypeScript Variables and Constants/2-Variables-and-Constants.ts
@@ -0,0 +1,17 @@
+// let age: number = 25;
+
+// Declare multiple variables in a single statement.
+
+let userName: string = "Aru", age: number = 20
+
+/**
+ * Recommended ways to name a variable in TypeScript.
+You can name variables any way you prefer. However, we recommend using the following naming conventions:
+
+In TypeScript, variables are generally named in camelCase format if they have multiple words.
+
+For example, firstName, annualSalary, numberOfBooks, etc.
+It's a good practice to give a descriptive name to a variable.
+
+For example, if you are using a variable to store the number of apples, it's better to name that variable apples or numberOfApples rather than x or n.
+ */
